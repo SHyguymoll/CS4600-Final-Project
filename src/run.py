7ee081c3825dfc49e195e63ac01fc219dea24ddc
SHyguymoll <71913506+SHyguymoll@users.noreply.github.com>
2023-12-04 16:23:30 -0800

    completed send def, starting on recieve

--- src/run.py
+++ src/run.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 AES_KEY_SIZE = 256
+SYSTEM_IV = modes.CBC(os.urandom(16))
 SYSTEM_HMAC_KEY = os.urandom(32)
 
 def main():
@@ -14,11 +15,10 @@ def main():
     
 
 def client():
+    rsa_key_pair : rsa.RSAPrivateKey
+    known_clients = []
     def __init__(self):
-        self.rsa_key_pair = rsa.generate_private_key(public_exponent=65537,key_size=2048)
-        self.connected_client : client
-        self.messages = []
-        self.known_clients = []
+        rsa_key_pair = rsa.generate_private_key(public_exponent=65537,key_size=2048)
     
     def get_pub_key(self) -> rsa.RSAPublicKey:
         return self.rsa_key_pair.public_key()
@@ -28,13 +28,18 @@ def client():
         if first:
             other.add_client(self, False)
     
+    def create_mac(*args):
+        hmac_sys = hmac.HMAC(SYSTEM_HMAC_KEY, hashes.SHA256())
+        for data in args: hmac_sys.update(data)
+        mac = hmac_sys.finalize()
+        return mac
+    
     def send_message(self, msg_location: str, recipient: client) -> None:
         if recipient not in self.known_clients:
             print(f"error, {recipient} is not known, aborting.")
             return
         # initializing and creating AES key with initialization vector (both random)
         picked_aes_key = algorithms.AES(os.urandom(AES_KEY_SIZE))
-        iv = modes.CBC(os.urandom(16))
         
         # opening message from file
         message_file = open(msg_location)
@@ -42,7 +47,7 @@ def client():
         message_file.close()
         
         # AES encryption of message, see https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
-        ciph = Cipher(picked_aes_key, iv)
+        ciph = Cipher(picked_aes_key, SYSTEM_IV)
         enc = ciph.encryptor()
         enc_message = enc.update(message) + enc.finalize()
         
@@ -56,12 +61,19 @@ def client():
         )
         
         # Creating HMAC of encrypted message and AES key
-        hmac_sys = hmac.HMAC(SYSTEM_HMAC_KEY, hashes.SHA256())
-        hmac_sys.update(enc_message)
-        hmac_sys.update(aes_transmit)
-        mac = hmac_sys.finalize()
+        mac = create_mac(enc_message, aes_transmit)
         # Sending message to recipient
-        recipient.messages.append([enc_message, aes_transmit, mac])
+        recipient.recieve_message((enc_message, aes_transmit, mac), self)
+    
+    def recieve_message(self, message_block: tuple, sender: client):
+        if sender not in self.known_clients:
+            print("Unknown sender! Aborting decryption.")
+        enc_msg, aes_trans, mac = message_block
+        comp_mac = create_mac(enc_msg, aes_trans)
+        if comp_mac != mac:
+            print("MAC comparison failed! Aborting decryption.")
+            return
+        
         
 
 if __name__ == "__main__":
